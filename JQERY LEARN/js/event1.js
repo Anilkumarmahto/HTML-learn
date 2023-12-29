58a6fa20258cf3335dd7f32411d8faa4599c4b43
Anil Kumar Mahto <107382907+Anilkumarmahto@users.noreply.github.com>
2023-12-29 22:35:12 +0530

    Delete JQERY LEARN directory

--- JQERY LEARN/js/event1.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $("#box").scroll(function(){
-    console.log("You are scrolling");
-  });
-
-  $(Window).resize(function(){
-    console.log("You are resizing window");
-  });
-});
